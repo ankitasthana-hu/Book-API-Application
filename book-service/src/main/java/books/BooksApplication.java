6970e8760ae722e552665abf47f446a30d62b482
anasthan <anasthan@PUNLPF0M3K53.corp.capgemini.com>
2020-07-05 01:34:15 +0200

    Commit 1#

---
+++ book-service/src/main/java/books/BooksApplication.java
@@ -0,0 +1,11 @@
+package books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksApplication {
+   public static void main(String[] args) {
+       SpringApplication.run(BooksApplication.class, args);
+   }
+}
